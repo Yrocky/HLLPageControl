551334c298dddb0e88411acfd5ab62133aa2eb37
Yrocky <983272765@qq.com>
2016-11-12 02:54:09 +0800

    Initial Commit

---
+++ HLLPageControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HLLPageControl
+//
+//  Created by Rocky Young on 16/11/12.
+//  Copyright © 2016年 HLL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HLLPageControl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HLLPageControl
+//
+//  Created by Rocky Young on 16/11/12.
+//  Copyright © 2016年 HLL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
